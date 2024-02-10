75da49553da67ba3b0d70a967f49f1ce5a5ce8ea
os-mhmd <docosmo010@gmail.com>
2024-02-10 19:00:50 +0200

    feat/main-website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,10 @@
-import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  imports: [RouterOutlet],
+  template: "<router-outlet></router-outlet>",
 })
-export class AppComponent {
-  slide = 1;
-  maxSlides = 8;
-
-  public ngOnInit(): void {
-    this.keyboardSwitcher();
-  }
-  nextSlide() {
-    this.slide++;
-  }
-  previousSlide() {
-    this.slide--;
-  }
-  switchSlide(slideNum: number) {
-    this.slide = slideNum;
-  }
-  active(slideNum: number) {
-    return slideNum == this.slide ? "active" : "";
-  }
-  slider(slideNum: number) {
-    return this.slide > slideNum
-      ? "previous"
-      : this.slide < slideNum
-      ? "next"
-      : "";
-  }
-  keyboardSwitcher() {
-    document.addEventListener("keydown", (e) => {
-      if (e.key == "ArrowRight") {
-        if (this.slide != this.maxSlides) this.slide++;
-      } else if (e.key == "ArrowLeft") {
-        if (this.slide != 1) this.slide--;
-      }
-    });
-  }
-}
+export class AppComponent {}
